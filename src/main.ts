3bd2ce2aafe7404dac4e1f33d140d5f02e2021b7
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-03-09 21:08:48 +0000

    Conceptos generales

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './style.css'
+//import './topics/01-basics-types';
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
---
+++ src/topics/01-basics-types.ts
@@ -0,0 +1,9 @@
+const nombre: string = 'Nicolás';
+let hp_points: number | string = 25;
+const esta_vivo: boolean = true;
+
+hp_points = 'Full';
+
+console.log(nombre, hp_points, esta_vivo);
+
+
