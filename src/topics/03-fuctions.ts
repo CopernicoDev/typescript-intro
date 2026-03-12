333a8de6480efa442ab784e64e90eb47dd2c6d88
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-03-12 21:27:54 +0000

    Probando cosas

--- src/topics/03-fuctions.ts
+++ src/topics/03-fuctions.ts
@@ -12,8 +12,29 @@ const ArrowFunction = (a: number, b: number) => {
     return `${a + b}`;
 }
 
-const result = addNumbers(1, 2);
-const result2 = multiplyNumbers(1, 2);
-const result3 = ArrowFunction(1, 2);
+//const result = addNumbers(1, 2);
+//const result2 = multiplyNumbers(1, 2);
+//const result3 = ArrowFunction(1, 2);
 
-console.log({ result, result2, result3 });
+//console.log({ result, result2, result3 });
+
+interface Personaje {
+    nombre: string;
+    pv: number;
+    showpv: () => void;
+}
+
+const personaje: Personaje = {
+    nombre: 'Nicolás',
+    pv: 100,
+    showpv() {
+        console.log(`Puntos de vida: ${this.pv}`);
+    }
+}
+
+const heal = (personaje: Personaje, amount: number) => {
+    personaje.pv += amount;
+    personaje.showpv();
+}
+
+heal(personaje, 15);
