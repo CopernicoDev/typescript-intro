b38d93241ece050f3aedcdf55a9df1ca4e05f0ac
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-03-12 21:36:49 +0000

    Probando lo aprendido

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 //import './topics/01-basics-types';
 //import './topics/02-object-interface';
-import './topics/03-fuctions';
+//import './topics/03-fuctions';
+import './topics/04-homework-types';
 
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
