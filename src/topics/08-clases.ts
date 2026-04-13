293c86e7e53182969c14be4bcd05005fd046d2d7
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-04-13 11:13:15 +0100

    Clases básicas 2

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -3,13 +3,17 @@ class Person {
     public age?: number;
     public profession?: string;
 
-    constructor() {
+    constructor(name: string, age: number, profession: string) {
         this.name = 'Nicolás';
         this.age = 25;
         this.profession = 'Developer';
     }
 }
 
-const ironman = new Person();
+const ironman = new Person('', 25, '');
+
+class Hero extends Person {
+
+}
 
 console.log(ironman);
