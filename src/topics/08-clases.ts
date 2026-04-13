4839628c03f0bc88ac7b7a7ad1430531597fe278
Nicolás Giraldo Muñoz <giraldomunoznicolas900@gmail.com>
2026-04-13 12:04:12 +0100

    Priorizar composición sobre herencia

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -16,10 +16,22 @@ class Hero extends Person {
     }
 }
 
-const ironman = new Person('Tony Stark', 45, 'Ingeniero');
+class HeroComposition {
+    constructor(
+        public alterEgo: string,
+        public realName: string,
+        public kills: number = 0, // <- Inicializado correctamente o asignándole un valor por defecto
+        public person: Person,
+    ) {
+    }
+}
+
+const person = new Person('Tony Stark', 45, 'Ingeniero');
+const ironman = new HeroComposition('Iron Man', 'Tony Stark', 0, person);
 
 const spiderman = new Hero('Spiderman', 'Peter Parker', 0);
 
+console.log(person);
 console.log(ironman);
 console.log(spiderman);
 
